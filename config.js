853cd81e78fdd29194eff5d7bbb75609fd321bf0
Srihari <srihari.sardena@intainft.com>
2019-10-19 13:19:56 +0530

    commit two

--- config.js
+++ config.js
@@ -15,5 +15,8 @@ hfc.setConfigSetting('network-connection-profile-path',path.join(__dirname, 'art
 hfc.setConfigSetting('Org1-connection-profile-path',path.join(__dirname, 'artifacts', 'org1.yaml'));
 hfc.setConfigSetting('Org2-connection-profile-path',path.join(__dirname, 'artifacts', 'org2.yaml'));
 hfc.setConfigSetting('Org3-connection-profile-path',path.join(__dirname, 'artifacts', 'org3.yaml'));
+hfc.setConfigSetting('Org4-connection-profile-path',path.join(__dirname, 'artifacts', 'org4.yaml'));
+hfc.setConfigSetting('Org5-connection-profile-path',path.join(__dirname, 'artifacts', 'org5.yaml'));
+hfc.setConfigSetting('Org6-connection-profile-path',path.join(__dirname, 'artifacts', 'org6.yaml'));
 // some other settings the application might need to know
 hfc.addConfigFile(path.join(__dirname, 'config.json'));
